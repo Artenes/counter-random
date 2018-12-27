a1d7bb3c10f96c75ae740918283da7f83b51d180
Artenes Nogueira <artenesama@gmail.com>
2018-12-26 20:53:09 -0400

    Makes button bigger, adds vibration and fixes oritentation change

--- app/src/main/java/io/github/artenes/counter/MainActivity.kt
+++ app/src/main/java/io/github/artenes/counter/MainActivity.kt
@@ -1,6 +1,9 @@
 package io.github.artenes.counter
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,9 +26,14 @@ class MainActivity : AppCompatActivity() {
         counterTextView.text = counter.toString()
     }
 
+    fun vibrate() {
+        getSystemService(Vibrator::class.java).vibrate(100)
+    }
+
     fun onCountClick(view: View) {
         counter++
         updateCouterTextView()
+        vibrate()
     }
 
     fun onClearClick(view: View) {
